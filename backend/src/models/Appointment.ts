8ecf5140f6b0ff1b08789565281926bda87655b8
Matheus <matheus8476@outlook.com>
2021-05-19 10:08:00 -0300

    feat(model): Transformando a classe Appointment em uma Entidade que tem relação com o banco

--- backend/src/models/Appointment.ts
+++ backend/src/models/Appointment.ts
@@ -1,21 +1,24 @@
-import { v4 as uuidv4 } from 'uuid';
+import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
 
+/*
+	O decorator @Entity funciona como uma função, a classe que está
+	abaixo dele funciona como um parâmetro a ser passado.
+	Tudo o que vier para essa classe será adicionado na tabela
+	appointments no banco.
+	A classe é exportada como uma entidade que tem relação direta com o banco
+	de dados.
+*/
+@Entity('appointments')
 class Appointment {
+	// Mapeando as colunas do banco, com atributos da classe
+	@PrimaryGeneratedColumn('uuid')
 	id: string;
 
+	@Column()
 	provider: string;
 
+	@Column('timestamp with time zone')
 	date: Date;
-
-	constructor({ provider, date }: Omit<Appointment, 'id'>) {
-		/*
-			Omit<Appointment, 'id'> indica que a função recebe todos os
-			atributos da classe exceto o id, Omit<Tipo, variável a ser omitida>
-		*/
-		this.id = uuidv4();
-		this.provider = provider;
-		this.date = date;
-	}
 }
 
 export default Appointment;
