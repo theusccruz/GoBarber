2189fec9ac73ee4289744fa58c686d899d040904
Matheus <matheus8476@outlook.com>
2021-05-19 10:05:50 -0300

    feat: Importando módulos e atualizando migration     -server e packase.json: Importando o módulo que habilita o uso de Decorators     -migration: Completando a descrição das outras colunas

--- backend/src/database/migrations/1621088993704-CreateAppointments.ts
+++ backend/src/database/migrations/1621088993704-CreateAppointments.ts
@@ -1,5 +1,12 @@
 import { MigrationInterface, QueryRunner, Table } from 'typeorm';
 
+/*
+	Migrations só podem ser alteradas enquanto não tiverem sido enviadas
+	para o controle de versão. Caso exista um erro em uma migration já
+	versionada, é necessário criar uma nova diferente para resolver o erro
+	da anterior.
+*/
+
 class CreateAppointments1621088993704 implements MigrationInterface {
 	public async up(queryRunner: QueryRunner): Promise<void> {
 		await queryRunner.createTable(
@@ -11,13 +18,26 @@ class CreateAppointments1621088993704 implements MigrationInterface {
 						type: 'varchar',
 						isPrimary: true,
 						generationStrategy: 'uuid',
+						default: 'uuid_generate_v4()',
+					},
+					{
+						name: 'provider',
+						type: 'varchar',
+						isNullable: false,
+					},
+					{
+						name: 'date',
+						type: 'timestamp with time zone', // ou soente timestamp em outros DB's
+						isNullable: false,
 					},
 				],
 			}),
 		);
 	}
 
-	public async down(queryRunner: QueryRunner): Promise<void> {}
+	public async down(queryRunner: QueryRunner): Promise<void> {
+		await queryRunner.dropTable('appointments');
+	}
 }
 
-// export default CreateAppointments1621088993704;
+export default CreateAppointments1621088993704;
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import routes from './routes/routes-index';
+import 'reflect-metadata'; // ativa o suporte aos decorators @
 
 import './database/index-db';
 
@@ -9,5 +10,5 @@ app.use(express.json());
 app.use(routes);
 
 app.listen(3333, () => {
-	console.log('Backend started 🔥');
+	console.log('Backend started 🔥🔥🔥');
 });
