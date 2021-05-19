4a2dade4b08c54b4e2afa511e64ff44e0504eb2e
Matheus <matheus8476@outlook.com>
2021-05-19 10:14:13 -0300

    feat(routes): appointment.routes foi remodelado com novos métodos de interação com o service e o repository

--- backend/src/routes/appointments.routes.ts
+++ backend/src/routes/appointments.routes.ts
@@ -1,31 +1,29 @@
+import { getCustomRepository } from 'typeorm';
 import { Router } from 'express';
 import { parseISO } from 'date-fns';
-import AppoinmentsRepository from '../repositories/AppoinmentsRepository';
+import AppointmentsRepository from '../repositories/AppointmentsRepository';
 import CreateAppointmentService from '../services/CreateAppointmentService';
 
 const appointmentsRouter = Router();
-const appointmentsRepository = new AppoinmentsRepository();
 
 /*
 	Aqui será verificado se tem mais alguma coisa (ex: parâmetros) pós o
 	"/appointments", se não tiver, /appointments vai se tornar o endereço raiz
 	nesse contexto
 */
-appointmentsRouter.get('/', (request, response) => {
-	const appointments = appointmentsRepository.listAppointments();
+appointmentsRouter.get('/', async (request, response) => {
+	const appointmentsRepository = getCustomRepository(AppointmentsRepository);
+	const appointments = await appointmentsRepository.find();
 	response.json(appointments);
 });
 
-appointmentsRouter.post('/', (request, response) => {
+appointmentsRouter.post('/', async (request, response) => {
 	const { provider, date } = request.body;
 	const parsedDate = parseISO(date);
-
-	const createAppointment = new CreateAppointmentService(
-		appointmentsRepository,
-	);
+	const createAppointment = new CreateAppointmentService();
 
 	try {
-		const appointment = createAppointment.execute({
+		const appointment = await createAppointment.execute({
 			provider,
 			date: parsedDate,
 		});
