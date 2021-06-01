9206e047809e846a8922215e12dcdce1cee91d5a
Matheus KBank <matheus8476@outlook.com>
2021-06-01 17:27:40 -0300

    refactor(frontend) Correções no código

--- frontend/src/components/Input/style.ts
+++ frontend/src/components/Input/style.ts
@@ -1,4 +1,3 @@
-import { Children } from 'react';
 import styled, { css } from 'styled-components';
 
 import Tooltip from '../Tooltip/Tooltip';
--- frontend/src/routes/Route.tsx
+++ frontend/src/routes/Route.tsx
@@ -1,4 +1,4 @@
-import React, { ReactComponentElement } from 'react';
+import React from 'react';
 import {
   Route as ReactDOMRoute,
   RouteProps as ReactDOMRouteProps,
