46a4881c69a8397120198ea767f9099371fd70d9
Matheus KBank <matheus8476@outlook.com>
2021-06-01 11:34:24 -0300

    feat(mobile) Adição de fontes/imagens/ícones

---
+++ mobile/react-native.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  project: {
+    ios: {},
+    android: {},
+  },
+  assets: ['./assets/fonts/'],
+};
+
+// comando yarn react-native link
+// leva as fontes para as pastas nativas ios e android
