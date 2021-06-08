310497c218bee177f0819c6da297b06ad605759d
Matheus KBank <matheus8476@outlook.com>
2021-06-08 18:00:09 -0300

    feat(backend): Criando o template para envio dos emails

--- backend/src/shared/providers/index.ts
+++ backend/src/shared/providers/index.ts
@@ -6,6 +6,14 @@ import DiskStorageProvider from './StorageProvider/implementations/DiskStoragePr
 import IMailProvider from './MailProvider/models/IMailProvider';
 import EtherialMailProvider from './MailProvider/implementations/EtherialMailProvider';
 
+import IMailTemplateProvider from './MailTemplateProvider/models/IMailTemplateProvider';
+import HandleBarsMailTemplateProvider from './MailTemplateProvider/implementations/HandleBarsMailTemplateProvider';
+
 container.registerSingleton<IStorageProvider>('StorageProvider', DiskStorageProvider);
 
 container.registerInstance<IMailProvider>('MailProvider', new EtherialMailProvider());
+
+container.registerSingleton<IMailTemplateProvider>(
+  'MailTemplateProvider',
+  HandleBarsMailTemplateProvider,
+);
