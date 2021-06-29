810b405449ac34f52387517f8a1a41158321d497
Matheus KBank <matheus8476@outlook.com>
2021-06-29 16:33:46 -0300

    feat: Construção de interface dos agendamentos + início do calendário

--- frontend/src/pages/Dashboard/index-dashboard.tsx
+++ frontend/src/pages/Dashboard/index-dashboard.tsx
@@ -1,12 +1,60 @@
-import React from 'react';
-import { FiPower } from 'react-icons/fi';
+import React, { useState, useCallback, useEffect } from 'react';
+import { FiClock, FiPower } from 'react-icons/fi';
+import DayPicker, { DayModifiers } from 'react-day-picker';
+import 'react-day-picker/lib/style.css';
 
-import { Container, Header, HeaderContent, Profile } from './styles';
+import {
+  Container,
+  Header,
+  HeaderContent,
+  Profile,
+  Content,
+  Schedule,
+  Calendar,
+  NextAppointment,
+  Section,
+  Appointment,
+} from './styles';
 import logoImg from '../../assets/logo.svg';
 import { useAuth } from '../../hooks/auth';
+import api from '../../services/api';
+
+type MonthAvailabilityItem = {
+  day: number;
+  available: boolean;
+};
 
 const Dashboard: React.FC = () => {
   const { user, signOut } = useAuth();
+
+  const [selectedDate, setSelectedDate] = useState(new Date());
+  const [currentMonth, setCurrentMonth] = useState(new Date());
+  const [monthAvailability, setMonthAvailability] = useState<MonthAvailabilityItem[]>([]);
+
+  const handleDateChange = useCallback((day: Date, modifiers: DayModifiers) => {
+    if (modifiers.available) {
+      setSelectedDate(day);
+    }
+  }, []);
+
+  const handleMonthChange = useCallback((month: Date) => {
+    setCurrentMonth(month);
+  }, []);
+
+  useEffect(() => {
+    async function getMonthAvailability() {
+      const response = await api.get(`/providers/${user.id}/month-availability`, {
+        params: {
+          yaer: currentMonth.getFullYear(),
+          month: currentMonth.getMonth() + 1,
+        },
+      });
+
+      setMonthAvailability(response.data);
+    }
+
+    getMonthAvailability();
+  }, [currentMonth, user.id]);
 
   return (
     <Container>
@@ -28,6 +76,124 @@ const Dashboard: React.FC = () => {
           </button>
         </HeaderContent>
       </Header>
+
+      <Content>
+        <Schedule>
+          <h1>Horários agendados</h1>
+
+          <p>
+            <span>Hoje</span>
+            <span>Dia 6</span>
+            <span>Segunda-feira</span>
+          </p>
+
+          <NextAppointment>
+            <strong>Atendimento a seguir</strong>
+            <div>
+              <img src="https://github.com/theusccruz.png" alt="User" />
+
+              <strong>Matheus Cruz</strong>
+              <span>
+                <FiClock />
+                08:00
+              </span>
+            </div>
+          </NextAppointment>
+
+          <Section>
+            <strong>Manhã</strong>
+
+            <Appointment>
+              <span>
+                <FiClock />
+                08:00
+              </span>
+
+              <div>
+                <img src="https://github.com/theusccruz.png" alt="User" />
+
+                <strong>Matheus Cruz</strong>
+              </div>
+            </Appointment>
+
+            <Appointment>
+              <span>
+                <FiClock />
+                08:00
+              </span>
+
+              <div>
+                <img src="https://github.com/theusccruz.png" alt="User" />
+
+                <strong>Matheus Cruz</strong>
+              </div>
+            </Appointment>
+          </Section>
+
+          <Section>
+            <strong>Tarde</strong>
+
+            <Appointment>
+              <span>
+                <FiClock />
+                08:00
+              </span>
+
+              <div>
+                <img src="https://github.com/theusccruz.png" alt="User" />
+
+                <strong>Matheus Cruz</strong>
+              </div>
+            </Appointment>
+
+            <Appointment>
+              <span>
+                <FiClock />
+                08:00
+              </span>
+
+              <div>
+                <img src="https://github.com/theusccruz.png" alt="User" />
+
+                <strong>Matheus Cruz</strong>
+              </div>
+            </Appointment>
+          </Section>
+        </Schedule>
+        <Calendar>
+          <DayPicker
+            weekdaysShort={['D', 'S', 'T', 'Q', 'Q', 'S', 'S']}
+            fromMonth={new Date()}
+            disabledDays={[
+              {
+                daysOfWeek: [0, 6],
+              },
+            ]}
+            selectedDays={selectedDate}
+            modifiers={{
+              available: {
+                daysOfWeek: [1, 2, 3, 4, 5],
+              },
+            }}
+            onMonthChange={handleMonthChange}
+            onDayClick={handleDateChange}
+            months={[
+              'Janeiro',
+              'Fevereiro',
+              'Março',
+              'Abril',
+              'Maio',
+              'Junho',
+              'Julho',
+              'Agosto',
+              'Setembro',
+              'Outubro',
+              'Novembro',
+              'Dezembro',
+            ]}
+          />
+        </Calendar>
+      </Content>
     </Container>
   );
 };
--- frontend/src/pages/Dashboard/styles.ts
+++ frontend/src/pages/Dashboard/styles.ts
@@ -1,6 +1,11 @@
 import styled from 'styled-components';
+import { shade } from 'polished';
+import ArrowLeftIcon from '../../assets/ArrowLeftIcon.svg';
+import ArrowRightIcon from '../../assets/ArrowRightIcon.svg';
 
-export const Container = styled.div``;
+export const Container = styled.div`
+  height: 100vh;
+`;
 
 export const Header = styled.header`
   padding: 32px 0;
@@ -59,6 +64,265 @@ export const Profile = styled.div`
 
     strong {
       color: #ff9000;
+    }
+  }
+`;
+
+export const Content = styled.main`
+  max-width: 1120px;
+  margin: 40px auto;
+
+  display: flex;
+`;
+
+export const Schedule = styled.div`
+  flex: 1;
+  margin-right: 90px;
+
+  h1 {
+    font-size: 36px;
+  }
+
+  p {
+    margin-top: 8px;
+    color: #ff9000;
+    display: flex;
+    align-items: center;
+    font-weight: 500;
+
+    span {
+      display: flex;
+      align-items: center;
+    }
+
+    span + span::before {
+      content: '';
+      width: 1px;
+      height: 12px;
+      background: #ff9000;
+      margin: 0 8px;
+    }
+  }
+`;
+
+export const NextAppointment = styled.div`
+  margin-top: 35px;
+
+  > strong {
+    color: #999591;
+    font-size: 20px;
+    font-weight: 400;
+  }
+
+  div {
+    background: #3e3b47;
+
+    display: flex;
+    align-items: center;
+
+    padding: 16px 24px;
+    border-radius: 10px;
+    margin-top: 25px;
+
+    position: relative;
+
+    &::before {
+      content: '';
+      position: absolute;
+      height: 80%;
+      width: 1px;
+      left: 0;
+      top: 10%;
+      background-color: #ff9000;
+    }
+
+    img {
+      width: 70px;
+      height: 70px;
+      border-radius: 50%;
+    }
+
+    strong {
+      margin-left: 24px;
+      color: #fff;
+    }
+
+    span {
+      margin-left: auto;
+
+      display: flex;
+      align-items: center;
+
+      color: #999591;
+
+      svg {
+        color: #ff9000;
+        margin-right: 8px;
+      }
+    }
+  }
+`;
+
+export const Section = styled.section`
+  margin-top: 40px;
+
+  > strong {
+    color: #999591;
+
+    font-size: 20px;
+    line-height: 26px;
+
+    border-bottom: 1px solid #3e3b46;
+    display: block;
+    padding-bottom: 13px;
+    margin-bottom: 13px;
+  }
+`;
+
+export const Appointment = styled.div`
+  display: flex;
+  align-items: center;
+
+  & + div {
+    margin-top: 12px;
+  }
+
+  span {
+    margin-left: auto;
+
+    display: flex;
+    align-items: center;
+
+    color: #f4ede8;
+
+    svg {
+      color: #ff9000;
+      margin-right: 8px;
+    }
+  }
+
+  div {
+    flex: 1;
+
+    background-color: #3e3b47;
+    display: flex;
+    align-items: center;
+    padding: 16px 24px;
+    border-radius: 10px;
+    margin-left: 24px;
+
+    img {
+      width: 56px;
+      height: 56px;
+      border-radius: 50%;
+    }
+
+    strong {
+      margin-left: 20px;
+      color: #fff;
+    }
+  }
+`;
+
+export const Calendar = styled.aside`
+  width: 350px;
+
+  .DayPicker {
+    border-radius: 10px;
+
+    &-wrapper {
+      padding-bottom: 0;
+      background: #28262e;
+      border-radius: 10px;
+      z-index: 0;
+    }
+
+    &-NavBar {
+      position: relative;
+
+      ::before {
+        content: '';
+        width: 100%;
+        height: 50px;
+        position: absolute;
+        background: #3e3b47;
+        border-radius: 10px 10px 0 0;
+        z-index: -1;
+      }
+    }
+
+    &-NavButton {
+      color: #999591 !important;
+      margin-top: 0;
+      top: 0;
+
+      &--prev {
+        background: url(${ArrowLeftIcon}) no-repeat center;
+        margin-right: 0;
+        left: 12px;
+        width: 50px;
+        height: 50px;
+      }
+
+      &--next {
+        background: url(${ArrowRightIcon}) no-repeat center;
+        right: 12px;
+        width: 50px;
+        height: 50px;
+      }
+    }
+
+    &-Month {
+      border-collapse: separate;
+      border-spacing: 8px;
+      margin: 0;
+      padding: 0 10px 10px;
+    }
+
+    &-Caption {
+      line-height: 50px;
+      color: #f4ede8;
+
+      > div {
+        text-align: center;
+      }
+    }
+
+    &-Weekday {
+      color: #666360;
+      font-size: 16px;
+    }
+
+    &-Day {
+      width: 40px;
+      height: 40px;
+      transition: all 0.2s ease;
+      border-radius: 10px;
+
+      &--today {
+        font-weight: normal;
+        color: #fff;
+      }
+
+      &--available:not(.DayPicker-Day--outside) {
+        background: #3e3b47;
+        border-radius: 10px;
+      }
+
+      &--disabled {
+        color: #666360;
+        background: transparent !important;
+      }
+
+      &--selected:not(.DayPicker-Day--disabled) {
+        background: #ff9000 !important;
+        color: #232129 !important;
+      }
+    }
+
+    &:not(.DayPicker--interactionDisabled)
+      .DayPicker-Day:not(.DayPicker-Day--disabled):not(.DayPicker-Day--selected):not(.DayPicker-Day--outside):hover {
+      background: ${shade(0.2, '#3e3b47')};
     }
   }
 `;
